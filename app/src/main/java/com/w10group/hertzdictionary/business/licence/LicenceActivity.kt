e1096d4a6878b378e1fbda2c16dbf3dea7a90389
qiaoyuang <qiaoyuang2012@gmail.com>
2018-06-25 23:47:45 +0800

    2018-06-25

--- app/src/main/java/com/w10group/hertzdictionary/business/licence/LicenceActivity.kt
+++ app/src/main/java/com/w10group/hertzdictionary/business/licence/LicenceActivity.kt
@@ -2,10 +2,18 @@ package com.w10group.hertzdictionary.business.licence
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 
 class LicenceActivity : AppCompatActivity() {
+
+    private lateinit var mRecyclerView: RecyclerView
+
+    private val titles = arrayOf("Anko", "CircleImageView", "Glide", "Gson", "OkHttp", "Retrofit", "RxAndroid", "RxJava", "RxKotlin", "Subsampling Scale Image View")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+
+
 }
