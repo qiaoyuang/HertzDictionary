18923e9994c515bbc788f72afaec56b83894597a
qiaoyuang <qiaoyuang2012@gmail.com>
2019-04-20 23:55:46 +0800

    update to Gradle 5.4

--- app/src/main/kotlin/com/w10group/hertzdictionary/core/CoroutinesBus.kt
+++ app/src/main/kotlin/com/w10group/hertzdictionary/core/CoroutinesBus.kt
@@ -15,30 +15,33 @@ import kotlin.reflect.KClass
 
 object CoroutinesBus {
 
-    val map = HashMap<KClass<*>, LinkedList<out Pair<OnWorkListener<*>, Channel<*>>>>()
-
-    fun <T : Any> put(key: KClass<T>, value: LinkedList<Pair<OnWorkListener<T>, Channel<T>>>) {
-        map[key] = value
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    fun <T : Any> get(key: KClass<T>) = map[key] as LinkedList<Pair<OnWorkListener<T>, Channel<T>>>?
-
     interface OnWorkListener<in T> {
         fun onWork(channel: ReceiveChannel<T>)
     }
 
-    inline fun <reified T : Any> register(observer: OnWorkListener<T>, capacity: Int = 0) {
-        var list = get(T::class)
+    private val map = HashMap<KClass<*>, LinkedList<out Pair<OnWorkListener<*>, Channel<*>>>>()
+
+    private fun <T : Any> put(key: KClass<T>, value: LinkedList<Pair<OnWorkListener<T>, Channel<T>>>) {
+        map[key] = value
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T : Any> get(key: KClass<T>) = map[key] as LinkedList<Pair<OnWorkListener<T>, Channel<T>>>?
+
+    fun <T : Any> register(clazz: KClass<T>, observer: OnWorkListener<T>, capacity: Int = 0) {
+        var list = get(clazz)
         if (list == null) {
             list = LinkedList()
-            put(T::class, list)
+            put(clazz, list)
         }
         list.add(observer to Channel(capacity))
     }
 
-    inline fun <reified T : Any> unRegister(observer: OnWorkListener<T>) {
-        val list = get(T::class)
+    inline fun <reified T : Any> register(observer: OnWorkListener<T>,
+                                          capacity: Int = 0) = register(T::class, observer, capacity)
+
+    fun <T : Any> unRegister(clazz: KClass<T>, observer: OnWorkListener<T>) {
+        val list = get(clazz)
         list?.let { linkedList ->
             linkedList.forEach {
                 if (it === observer) {
@@ -47,15 +50,19 @@ object CoroutinesBus {
                 }
             }
             if (linkedList.isEmpty()) {
-                map.remove(T::class)
+                map.remove(clazz)
             }
         }
     }
 
-    inline fun <reified T : Any> CoroutineScope.post(event: T) = get(T::class)?.forEach {
+    inline fun <reified T : Any> unRegister(observer: OnWorkListener<T>) = unRegister(T::class, observer)
+
+    fun <T : Any> CoroutineScope.post(clazz: KClass<T>, event: T) = get(clazz)?.forEach {
         val (onWorkListener, channel) = it
         launch { channel.send(event) }
         onWorkListener.onWork(channel)
     }
+
+    inline fun <reified T : Any> CoroutineScope.post(event: T) = post(T::class, event)
 
 }
