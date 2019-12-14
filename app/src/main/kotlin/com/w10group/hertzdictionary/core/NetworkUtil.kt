092fa8a2f4b2e54dd7c7f78fd91c39e79c6a0622
qiaoyuang <qiaoyuang2012@gmail.com>
2019-12-14 09:52:33 +0800

    Use Ktor instead of Retrofit

--- app/src/main/kotlin/com/w10group/hertzdictionary/core/NetworkUtil.kt
+++ app/src/main/kotlin/com/w10group/hertzdictionary/core/NetworkUtil.kt
@@ -1,38 +1,21 @@
 package com.w10group.hertzdictionary.core
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import com.w10group.hertzdictionary.biz.manager.NetworkService
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.util.KtorExperimentalAPI
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-import java.lang.ref.SoftReference
 
 /**
  * Created by Administrator on 2018/2/6 0006.
- * 网络工具类
+ * Ktor HttpClient
  */
 
-object NetworkUtil {
-
-    private const val BASE_URL = "http://translate.google.cn/"
-    private const val MEDIA_TYPE_JSON = "application/json"
-
-    @UseExperimental(UnstableDefault::class)
-    private val mRetrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(Json.nonstrict.asConverterFactory(MEDIA_TYPE_JSON.toMediaType()))
-            .build()
-
-    private var mInstanceSoftReference = initInstanceSoftReference()
-
-    val instance: NetworkService
-        get() {
-            if (mInstanceSoftReference.get() == null)
-                mInstanceSoftReference = initInstanceSoftReference()
-            return mInstanceSoftReference.get()!!
-        }
-
-    private fun initInstanceSoftReference() = SoftReference(mRetrofit.create(NetworkService::class.java))
-
+@UseExperimental(KtorExperimentalAPI::class, UnstableDefault::class)
+val CLIENT = HttpClient(OkHttp) {
+    install(JsonFeature) {
+        serializer = KotlinxSerializer(Json.nonstrict)
+    }
 }
