1871f9384a61f718cd9800c28a7dd19adc1f9bec
qiaoyuang <qiaoyuang2012@gmail.com>
2020-03-22 23:21:29 +0800

    Add some notation

--- data/src/iosMain/kotlin/com.w10group.hertzdictionary/core/NetworkUtilIos.kt
+++ data/src/iosMain/kotlin/com.w10group.hertzdictionary/core/NetworkUtilIos.kt
@@ -3,6 +3,11 @@ package com.w10group.hertzdictionary.core
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.ios.Ios
 
+/**
+ * iOS 端 Ktor HttpClient
+ * @author qiaoyuang
+ */
+
 actual val CLIENT = nsUrlSession()
 
 private fun nsUrlSession(): HttpClient = HttpClient(Ios) {
